44847fcc8cc35c98ecb60bae63a6f8a8b408f78f
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2026-03-06 08:32:13 -0800

    create dead component

---
+++ scenes/component/DeadComponent.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+public partial class DeadComponent : Node2D
+{
+	[Export]
+	private float decomposeDelayInSeconds = 5.0f;
+	private AnimatedSprite2D sprite;
+
+	public override async void _Ready()
+	{
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		await ToSignal(GetTree().CreateTimer(decomposeDelayInSeconds), SceneTreeTimer.SignalName.Timeout);
+
+		sprite.Play("decompose");
+
+		await ToSignal(sprite, AnimatedSprite2D.SignalName.AnimationFinished);
+
+		QueueFree();
+	}
+}
--- scenes/component/HealthComponent.cs
+++ scenes/component/HealthComponent.cs
@@ -10,21 +10,30 @@ public partial class HealthComponent : Node2D
 	public override void _Ready()
 	{
 		healthProgressBar = GetNode<ProgressBar>("%HealthProgressBar");
+		Visible = false;
 	}
 
 	public void SetMaxValue(int health)
 	{
 		healthProgressBar.MaxValue = health;
+		SetHealthBarVisibility();
 	}
 
 	public void SetCurrentHealth(int currentHealth)
 	{
 		healthProgressBar.Value = currentHealth;
+		SetHealthBarVisibility();
 	}
 
 	public void AddToCurrentHealth(int healthToAdd)
 	{
 		healthProgressBar.Value += healthToAdd;
+		SetHealthBarVisibility();
+	}
+
+	private void SetHealthBarVisibility()
+	{
+		Visible = healthProgressBar.Value != healthProgressBar.MaxValue;
 	}
 
 
