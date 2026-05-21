0171c1d820a66e54dd67377adff87c9ca1a996f1
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2026-05-21 10:24:34 -0700

    reonotialize of grid manager

--- scenes/manager/GridManager.cs
+++ scenes/manager/GridManager.cs
@@ -1,122 +1,66 @@
-using System.Collections.Generic;
-using System.Linq;
-using Game.Autoload;
-using Game.Component;
+using System.Reflection.Metadata.Ecma335;
 using Godot;
 
 namespace Game.Manager;
 
-public partial class GridManager : Node
+public partial class GridManager
 {
-	private const int CELL_SIZE = 64;
-	
-	private HashSet<Vector2I> occupiedCells = [];
-
-	[Export]
-	private TileMapLayer highlightTilemapLayer;
-	[Export]
-	private TileMapLayer baseTerrainTilemapLayer;
-
-	private List<TileMapLayer> allTilemapLayers = [];
-
-	public override void _Ready()
+	public enum CellState
 	{
-		GameEvents.Instance.BuildingPlaced += OnBuildingPlaced;
-		UnitsEvents.Instance.UnitMovementFinsished += OnUnitMovementFinished;
-		UnitsEvents.Instance.UnitMovementStarted += OnUnitMovementStarted;
-
-		allTilemapLayers = GetAllTilemapLayers(baseTerrainTilemapLayer);
+		Empty,
+		Unit,
+		Resource,
+		Building
 	}
 
-	public bool IsTilePositionValid(Vector2I tilePosition)
+	public const int CELL_SIZE = 64;
+	public int GridWidth { get; private set; }
+	public int GridHeight { get; private set; }
+
+	private (CellState cellState, Node2D node)[,] grid;
+
+	public GridManager(int mapWidth, int mapHeight)
 	{
-		foreach(var layer in allTilemapLayers)
+		GridWidth = mapWidth;
+		GridHeight = mapHeight;
+		grid = new (CellState, Node2D)[GridWidth, GridHeight];
+		InitializeGrid();
+	}
+
+	public bool IsCellFree(int x, int y) => grid[x, y].cellState == CellState.Empty;
+
+	public bool IsCellWithinBounds(int x, int y) => x >= 0 && x < GridWidth && y >= 0 && y < GridHeight;
+
+	public static Vector2 WorldPositionToGridCell(Vector2 worldPos)
+	{
+		var x = Mathf.Floor(worldPos.X / CELL_SIZE);
+		var y = Mathf.Floor(worldPos.Y / CELL_SIZE);
+		return new Vector2(x, y);
+	}
+
+	public bool TryRegisterEntityAtCell(Vector2 cellPos, CellState state, Node2D entityNode)
+	{
+		var cellPosition = WorldPositionToGridCell(cellPos);
+		var x = (int)cellPosition.X;
+		var y = (int)cellPosition.Y;
+
+		if(!IsCellFree(x, y))
 		{
-			var customData = layer.GetCellTileData(tilePosition);
-			if(customData == null) continue;
-			return (bool)customData.GetCustomData("buildable");
+			return false;
 		}
-		return false;
+
+		grid[x, y] = (state, entityNode);
+		return true;
 	}
 
-	public void MarkTileAsOccupied(Vector2I tilePosition) => occupiedCells.Add(tilePosition);
-
-	public void MarkTileAsUnoccupied(Vector2I tilePosition) => occupiedCells.Remove(tilePosition);
-
-	public bool IsTileOccupied(Vector2I cellPosition) => occupiedCells.Contains(cellPosition);
-
-	public void HighlightBuildableTiles()
+	private void InitializeGrid()
 	{
-		ClearHighlightedTiles();
-		var buildingComponents = GetTree().GetNodesInGroup(nameof(BuildingComponent)).Cast<BuildingComponent>();
-		foreach(var buildingComponent in buildingComponents)
+		for (int x = 0; x < GridWidth; x++)
 		{
-			HighlightValidTilesInRadius(buildingComponent.GetGridCellPosition(), buildingComponent.BuildableRadius);
-		}
-	}
-
-	public void ClearHighlightedTiles()
-	{
-		highlightTilemapLayer.Clear();
-	}
-
-	public Vector2I GetMouseGridCellPosition()
-	{
-		Vector2 mousePosition = highlightTilemapLayer.GetGlobalMousePosition();
-		return ConvertWorldPositionToTilePosition(mousePosition);
-	}
-
-	public static Vector2I ConvertWorldPositionToTilePosition(Vector2 worldPosition)
-	{
-		Vector2 tilePosition = (worldPosition / 64).Floor();
-		return new Vector2I((int)tilePosition.X, (int)tilePosition.Y);
-	}
-
-	private List<TileMapLayer> GetAllTilemapLayers(TileMapLayer rootTilemapLayer)
-	{
-		var result = new List<TileMapLayer>();
-		var children = rootTilemapLayer.GetChildren();
-		children.Reverse();
-		foreach(var child in children)
-		{
-			if(child is TileMapLayer childLayer)
+			for (int y = 0; y < GridHeight; y++)
 			{
-				result.AddRange(GetAllTilemapLayers(childLayer));
-			}
-		}
-		result.Add(rootTilemapLayer);
-		return result;
-	}
-
-	private void HighlightValidTilesInRadius(Vector2I rootCell, int radius)
-	{
-		for (var x = rootCell.X - radius; x <= rootCell.X + radius; x++)
-		{
-			for (var y = rootCell.Y - radius; y <= rootCell.Y + radius; y++)
-			{
-				Vector2I tilePosition = new(x, y);
-				if(!IsTilePositionValid(tilePosition)) 
-				{
-					continue;
-				}
-				highlightTilemapLayer.SetCell(tilePosition, 0, Vector2I.Zero);
+				grid[x, y] = (CellState.Empty, null);
 			}
 		}
 	}
-
-	private void OnBuildingPlaced(BuildingComponent buildingComponent)
-	{
-		MarkTileAsOccupied(buildingComponent.GetGridCellPosition());
-	}
-
-	private void OnUnitMovementFinished(Vector2 globalPosition)
-	{
-		MarkTileAsOccupied(ConvertWorldPositionToTilePosition(globalPosition));
-	}
-
-	private void OnUnitMovementStarted(Vector2 globalPosition)
-	{
-		MarkTileAsUnoccupied(ConvertWorldPositionToTilePosition(globalPosition));
-	}
-
 }
